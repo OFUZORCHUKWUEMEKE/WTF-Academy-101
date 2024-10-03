8976561f9d68840ea23127a38f9bfbd77ee958c6
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-03 22:32:57 +0100

    mappings

---
+++ 101/mappings.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27;
+
+contract Mappings{
+    mapping(uint=>address) public idToAddress;
+    mapping(address => address) public swapPair;
+
+    strcut Student{
+        uint256 id;
+        uint256 score;
+    }
+
+    mapping(Student => uint) public testVar;
+
+    
+}
