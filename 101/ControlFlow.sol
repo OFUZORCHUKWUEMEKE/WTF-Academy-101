6565da9ba533a99a4b5d2af492d27ba9ea3d9a83
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-04 21:38:04 +0100

    control flow

---
+++ 101/ControlFlow.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27;
+
+Contract ControlFlow{
+    uint public sum = 0;
+    function IfelseTest(uint256 _number) public pure returns(bool){
+        if(_number==0){
+            return(true); 
+        }else{
+            return(false);
+        }
+    }
+
+    function forLoopTest() public pure returns(uint256){
+        for(uint i =0; i<10; i++){
+            sum+=i;
+        }
+        return(sum);
+    }
+
+    function whileTest() public pure returns(uint256){
+        uint sum = 0;
+        uint i = 0 ;
+        while(i <10){
+            sum+=i;
+            i++;
+        }
+        return(sum)
+    }
+
+    function ternaryTest(uint256 x, uint256 y) public pure returns(uint256){
+    // return the max of x and y
+    return x >= y ? x: y; 
+    }
+}
